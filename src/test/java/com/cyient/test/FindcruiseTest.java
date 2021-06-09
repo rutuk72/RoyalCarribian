d272db2f7ead58dd3f016d6334ba9c3a580cd66a
rk59112 <rk59112@MKD-CORP-BCP112.CORP.CYIENT.COM>
2021-06-09 10:11:29 +0530

    third commit

--- src/test/java/com/cyient/test/FindcruiseTest.java
+++ src/test/java/com/cyient/test/FindcruiseTest.java
@@ -1,21 +1,22 @@
-package com.cyient.base;
+package com.cyient.test;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
-public class FindCruise extends BrowserBase {
+import com.cyient.base.BrowserBase;
 
+public class FindcruiseTest extends BrowserBase  {
 	@Test
-	public void findCruise()  {
+	public void findCruise() {
 
 		
 		driver.findElement(By.xpath("//div[@class='email-capture__body']//*[local-name()='svg']")).click();
 		driver.findElement(By.xpath("//button[@id='rciHeaderOpenSidenav']")).click();
 		driver.findElement(By.xpath("//a[@id='rciHeaderSideNavMenu-1']")).click();
 		driver.findElement(By.xpath("//span[contains(text(),'2021-2022 Cruises')]")).click();
-		driver.findElement(By.xpath("//input[@id='rciFirstName']")).sendKeys("Sahil");
-		driver.findElement(By.xpath("//input[@id='rciLastName']")).sendKeys("Karnewar");
+		driver.findElement(By.xpath("//input[@id='rciFirstName']")).sendKeys("rutwik");
+		driver.findElement(By.xpath("//input[@id='rciLastName']")).sendKeys("Kulkarni");
 		Select selectCountry = new Select(driver.findElement(By.xpath("//select[@class='md-input']")));
 		selectCountry.selectByVisibleText("India");
 		driver.findElement(By.xpath("//button[normalize-space()='Sign Up']")).click();
