6ead0cd3c6aae2fae00751ca79c31c62853a7f7c
rk59112 <rk59112@MKD-CORP-BCP112.CORP.CYIENT.COM>
2021-06-09 10:06:46 +0530

    second

--- src/test/java/com/cyient/test/SignupTest.java
+++ src/test/java/com/cyient/test/SignupTest.java
@@ -2,15 +2,12 @@ package com.cyient.test;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.cyient.base.BrowserBase;
 
-
+@Test(priority = 1)
 public class SignupTest extends BrowserBase{
-	
-	@Test(priority = 1)
 	public void test() throws InterruptedException {
 	driver.findElement(By.xpath("//span[contains(text(),'Sign In')]")).click();
 	driver.findElement(By.xpath("//a[normalize-space()='Create an account']")).click();
@@ -47,28 +44,5 @@ public class SignupTest extends BrowserBase{
 
 	
 	}
-	
-	
-	@Test(priority = 2)
-	public void Test() throws InterruptedException {
-		Thread.sleep(1000);
-		//driver.findElement(By.xpath("//img[@class='highlight__content']/..")).click();
-		driver.findElement(By.xpath("//div[@class='brand__logo-redirection']")).click();
-		Thread.sleep(1000);
-		driver.findElement(By.xpath("//div[@class='headerMainToolbar__menu__icon']")).click();
-		driver.findElement(By.xpath("//span[normalize-space()='Find a Cruise']")).click();
-		driver.findElement(By.xpath("//span[normalize-space()='2021-2022 Cruises']")).click();
-		
-		driver.findElement(By.id("rciFirstName")).sendKeys("Dennis");
-		driver.findElement(By.id("rciLastName")).sendKeys("Rich");
-		driver.findElement(By.xpath("//select[@class='md-input']")).click();
-		driver.findElement(By.xpath("//option[@value='IND']")).click();
-		driver.findElement(By.xpath("//button[normalize-space()='Sign Up']")).click();
-
-		String actalValue= driver.findElement(By.id("rciEmailAddressMissing")).getText();
-		Assert.assertEquals( actalValue,"Missing Email Address");
-		
-	}
-	
 
 }
